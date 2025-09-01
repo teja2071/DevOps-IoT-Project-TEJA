1ddde319de246f6bb46abcd5a68fd20e4b2fd021
teja2071 <108168128+teja2071@users.noreply.github.com>
2025-09-01 03:39:35 -0400

    Create app.java

---
+++ SRC/main/java/com/iot/app.java
@@ -0,0 +1,11 @@
+package com.iot;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("IoT Application Running...");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
